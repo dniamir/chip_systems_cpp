566ddf7bafa1a35644c38652f6af243bcdbe9da1
Darien Niamir <darien.niamir@gmail.com>
2021-12-22 22:35:37 -0800

    ICM20649 updates
    - Using chip base class
    - Add all registers
    - Functions for setting up axes for measurements
    - Add FIFO setup and usage

--- icm20649.cpp
+++ icm20649.cpp
@@ -1,21 +1,27 @@
 # include <icm20649.h>
 # include <Arduino.h>
 # include <arduino_i2c.h>
+# include <cmath>
 
-ICM20649::ICM20649(ArduinoI2C &comm_protocol){
-
-    // static Protocol comm_protocol = comm_protocol;
-
+ICM20649::ICM20649(int i2c_address_in, ArduinoI2C input_protocol) : Chip(i2c_address_in, input_protocol) {
+    Chip::field_map = field_map;
+    Chip::who_am_i_reg = who_am_i_reg;
 };
 
-bool ICM20649::initialize(void) {
+ICM20649::ICM20649(ArduinoI2C input_protocol) : Chip(input_protocol){
+    Chip::field_map = field_map;
+    Chip::who_am_i_reg = who_am_i_reg;
+    Chip::i2c_address = i2c_address;
+};
+
+bool ICM20649::initialize() {
 
     // Check device_id
-    uint8_t icm_check = comm_protocol.read_register(0x68, 0x00);
+    uint8_t icm_check = read_field("WHO_AM_I");
 
     bool icm_ok = false;
 
-    if (icm_check == 0xE1) {
+    if (icm_check == who_am_i_val) {
         Serial.println("ICM20649 connection was successful");
         icm_ok = true;
     }
@@ -25,4 +31,203 @@ bool ICM20649::initialize(void) {
     }
 
     return icm_ok;
+}
+
+void ICM20649::default_mode() {
+    
+    write_field("DEVICE_RESET", 1);
+    delay(200);
+    
+    write_field("USER_BANK", 0);
+    write_field("SLEEP", 0);
+    write_field("DISABLE_ACCEL", 0);
+    write_field("DISABLE_GYRO", 0);
+    delay(200);
+    
+    setup_gyro();
+    setup_accel();
+    setup_temperature();
+
+}
+
+void ICM20649::setup_gyro() {
+
+    write_field("USER_BANK", 2);
+
+    // Full Scale 
+    write_field("GYRO_FS_SEL", 0);  // +/- 500dps
+
+    // Low Pass Filter
+    write_field("GYRO_FCHOICE", 1); // Low noise mode, DLPF eneabled
+    write_field("GYRO_DLPFCFG", 1); // 3dB bandwidth of 150 Hz
+
+    // Averaging - 2 ^ x averaging
+    write_field("GYRO_AVGCFG", 3);  // 8x averaging
+
+    // ODR 1125/(1+GYRO_SMPLRT_DIV)Hz 
+    write_field("GYRO_SMPLRT_DIV", 10); // Roughly 102Hz ODR
+    delay(200);
+}
+
+void ICM20649::setup_accel() {
+
+    write_field("USER_BANK", 2);
+
+    // Full Scale
+    write_field("ACCEL_FS_SEL", 0);  // +/- 4G
+
+    // Low Pass Filter
+    write_field("ACCEL_FCHOICE", 1);    // Low noise mode, enable DLPF
+    write_field("ACCEL_DLPFCFG", 7);    // 3dB bandwidth of 473 Hz 
+    // write_field("ACCEL_DLPFCFG", 1);    // 3dB bandwidth of 246 Hz 
+
+    // Averages
+    write_field("DEC3_CFG", 3);  // 32x averages 
+
+    // ODR 1125/(1+ACCEL_SMPLRT_DIV)Hz - Comes out to about 102Hz
+    write_field("ACCEL_SMPLRT_DIV_1", 0);  // 4 MSB bits of ACCEL_SMPLRT_DIV
+    write_field("ACCEL_SMPLRT_DIV_2", 10);  // 8 LSB bits of ACCEL_SMPLRT_DIV
+    delay(200);
+}
+
+void ICM20649::setup_temperature() {
+    write_field("TEMP_DLPFCFG", 2); // 1.125kHz ODr and 123Hz BW
+}
+
+void ICM20649::setup_fifo_6axis() {
+
+    write_field("USER_BANK", 0);
+
+    // Turn on all 6-axes
+    write_field("ACCEL_FIFO_EN", 1);
+    write_field("GYRO_Z_FIFO_EN", 1);
+    write_field("GYRO_Y_FIFO_EN", 1);
+    write_field("GYRO_X_FIFO_EN", 1);
+    write_field("TEMP_FIFO_EN", 1);
+
+    write_field("FIFO_MODE", 0);  // Will continue writing to FIFO when full
+
+    write_field("FIFO_EN", 1);  // Enable FIFO
+
+}
+
+uint16_t ICM20649::read_fifo_count() {
+
+    uint8_t fifo_count_array[2];
+
+    write_field("USER_BANK", 0);
+    read_field("FIFO_COUNTH", 2, fifo_count_array);
+
+    uint16_t fifo_count = (fifo_count_array[0] << 8) | fifo_count_array[1];
+
+    return fifo_count;
+}
+
+void ICM20649::read_fifo() {
+
+    // Read the number of entries in the FIFO
+    uint16_t fifo_count = read_fifo_count();
+    fifo_count = fifo_count / 14;
+
+    // Initialize data to read from FIFO
+    uint8_t register_out[fifo_count];
+    float imu_data[7][fifo_count];
+
+    // Batch read from the FIFO and process the data
+    for (int i = 0; i < fifo_count; i++) {
+        read_field("FIFO_R_W", 14, register_out);
+
+        imu_data[0][i] = process_accel(register_out[0], register_out[1], 4000);
+        imu_data[1][i] = process_accel(register_out[2], register_out[3], 4000);
+        imu_data[2][i] = process_accel(register_out[4], register_out[5], 4000);
+
+        imu_data[3][i] = process_gyro(register_out[6], register_out[7], 50);
+        imu_data[4][i] = process_gyro(register_out[8], register_out[9], 50);
+        imu_data[5][i] = process_gyro(register_out[10], register_out[11], 50);
+
+        imu_data[6][i] = process_temperature(register_out[12], register_out[13]);
+    }
+
+    // Average data from FIFO
+    float ax_average = average(imu_data[0], (int)fifo_count);
+    float ay_average = average(imu_data[1], (int)fifo_count);
+    float az_average = average(imu_data[2], (int)fifo_count);
+    float gx_average = average(imu_data[3], (int)fifo_count);
+    float gy_average = average(imu_data[4], (int)fifo_count);
+    float gz_average = average(imu_data[5], (int)fifo_count);
+    float temperature_average = average(imu_data[6], (int)fifo_count);
+    
+    Serial.println();
+    Serial.print(ax_average); 
+    Serial.print("    ");
+    Serial.print(ay_average);
+    Serial.print("    ");
+    Serial.print(az_average); 
+    Serial.print("    ");
+    Serial.print(gx_average); 
+    Serial.print("    ");
+    Serial.print(gy_average);
+    Serial.print("    ");
+    Serial.print(gz_average); 
+    Serial.print("    ");
+    Serial.print(temperature_average); 
+    Serial.print("    ");
+    Serial.print((int)fifo_count); 
+
+}
+
+void ICM20649::read_axyz_gxyz() {
+
+    uint8_t register_out[14];
+
+    // Read all registers
+    write_field("USER_BANK", 0);
+    read_field("ACCEL_XOUT_H", 13, register_out);
+
+    float ax_mgee = process_accel(register_out[0], register_out[1], 4000);
+    float ay_mgee = process_accel(register_out[2], register_out[3], 4000);
+    float az_mgee = process_accel(register_out[4], register_out[5], 4000);
+
+    float gx_dps = process_gyro(register_out[6], register_out[7], 50);
+    float gy_dps = process_gyro(register_out[8], register_out[9], 50);
+    float gz_dps = process_gyro(register_out[10], register_out[11], 50);
+
+    float temperature_degc = process_temperature(register_out[12], register_out[13]);
+
+    Serial.println();
+    Serial.print(ax_mgee); 
+    Serial.print("    ");
+    Serial.print(ay_mgee);
+    Serial.print("    ");
+    Serial.print(az_mgee); 
+    Serial.print("    ");
+    Serial.print(gx_dps); 
+    Serial.print("    ");
+    Serial.print(gy_dps);
+    Serial.print("    ");
+    Serial.print(gz_dps); 
+    Serial.print("    ");
+    Serial.print(temperature_degc); 
+
+}
+
+float ICM20649::process_accel(uint8_t accel_msb, uint8_t accel_lsb, int afs_fsr) {
+
+    int16_t accel_lsbs = (accel_msb << 8) | accel_lsb;
+    float accel_mgee = accel_lsbs * afs_fsr / pow(2, 15);
+    return accel_mgee;
+}
+
+float ICM20649::process_gyro(uint8_t gyro_msb, uint8_t gyro_lsb, int gfs_fsr) {
+
+    int16_t gyro_lsbs = (gyro_msb << 8) | gyro_lsb;
+    float gyro_dps = gyro_lsbs * gfs_fsr / pow(2, 15);
+    return gyro_dps;
+}
+
+float ICM20649::process_temperature(uint8_t temperature_msb, uint8_t temperature_lsb) {
+
+    uint16_t temperature_lsbs = (temperature_msb << 8) + temperature_lsb;
+    float temperature_degc = (temperature_lsbs - 21) / 333.87 + 15;
+    return temperature_degc;
 }
--- icm20649.h
+++ icm20649.h
@@ -1,15 +1,151 @@
 # include <protocol.h>
+# include <Arduino.h>
 # include <arduino_i2c.h>
+# include <chip.h>
 
-class ICM20649 {
+struct icm20649_trim_registers {
+      int8_t dig_x1;
+      int8_t dig_y1;
+      int8_t dig_x2;
+      int8_t dig_y2;
+      uint16_t dig_z1;
+      int16_t dig_z2;
+      int16_t dig_z3;
+      int16_t dig_z4;
+      uint8_t dig_xy1;
+      int8_t dig_xy2;
+      uint16_t dig_xyz1;
+    };
+
+class ICM20649 : public Chip {
 
   public:
-    ICM20649(ArduinoI2C &comm_protocol);
-    ArduinoI2C comm_protocol;
-    /**
-        \brief initialze device
+  // Different constructors
+    ICM20649(int i2c_address_in, ArduinoI2C input_protocol);
+    ICM20649(ArduinoI2C input_protocol);
 
-    */
-    bool initialize(void);
+    // Dfault I2C address
+    int i2c_address = 0b1101000;
+
+    // WHO AM I register - register the check upon startup
+    Field who_am_i_reg = Field{0x40, 0, 8, false};
+
+    // Initialize the device
+    bool initialize();
+
+    // Put the IMU in default mode
+    void default_mode();
+
+    // Setup the different on-chip sensors
+    void setup_gyro();
+    void setup_accel();
+    void setup_temperature();
+
+    // Fifo functions
+    void setup_fifo_6axis();
+    uint16_t read_fifo_count();
+    void read_fifo();
+
+    // Read all Accel and Gyro Axes
+    void read_axyz_gxyz();
+
+    // Process data
+    float process_accel(uint8_t accel_msb, uint8_t accel_lsb, int afs_fsr);
+    float process_gyro(uint8_t gyro_msb, uint8_t gyro_lsb, int gfs_fsr);
+    float process_temperature(uint8_t temperature_msb, uint8_t temperature_lsb);
+
+  private:
+
+    // Value in default WHO AM I register - value to match during startup check
+    int who_am_i_val = 0xE1;
+
+  protected:
+
+  // Register map
+  public:
+
+  // Register Map
+  std::map<String, Field> field_map {
+    
+    {"WHO_AM_I", Field{0x00, 0, 8, false}},
+    {"USER_CTRL", Field{0x03, 0, 8, false}},
+    {"DMP_EN", Field{0x03, 7, 1, false}},
+    {"FIFO_EN", Field{0x03, 6, 1, false}},
+    {"I2C_MST_EN", Field{0x03, 5, 1, false}},
+    {"I2C_IF_DIS", Field{0x03, 4, 1, false}},
+    {"DMP_RST", Field{0x03, 3, 1, false}},
+    {"SRAM_RST", Field{0x03, 2, 1, false}},
+    {"I2C_MST_RST", Field{0x03, 1, 1, false}},
+    {"LP_CONFIG", Field{0x05, 0, 8, false}},
+    {"I2C_MST_CYCLE", Field{0x05, 6, 1, false}},
+    {"ACCEL_CYCLE", Field{0x05, 5, 1, false}},
+    {"GYRO_CYCLE", Field{0x05, 4, 1, false}},
+    {"PWR_MGMT_1", Field{0x06, 0, 8, false}},
+    {"DEVICE_RESET", Field{0x06, 7, 1, false}},
+    {"SLEEP", Field{0x06, 6, 1, false}},
+    {"LP_EN", Field{0x06, 5, 1, false}},
+    {"TEMP_DIS", Field{0x06, 3, 1, false}},
+    {"CLKSEL", Field{0x06, 0, 3, false}},
+    {"PWR_MGMT_2", Field{0x07, 0, 8, false}},
+    {"DISABLE_ACCEL", Field{0x07, 3, 3, false}},
+    {"DISABLE_GYRO", Field{0x07, 0, 3, false}},
+    {"ACCEL_XOUT_H", Field{0x2D, 0, 8, false}},
+    {"ACCEL_XOUT_L", Field{0x2E, 0, 8, false}},
+    {"ACCEL_YOUT_H", Field{0x2F, 0, 8, false}},
+    {"ACCEL_YOUT_L", Field{0x30, 0, 8, false}},
+    {"ACCEL_ZOUT_H", Field{0x31, 0, 8, false}},
+    {"ACCEL_ZOUT_L", Field{0x32, 0, 8, false}},
+    {"GYRO_XOUT_H", Field{0x33, 0, 8, false}},
+    {"GYRO_XOUT_L", Field{0x34, 0, 8, false}},
+    {"GYRO_YOUT_H", Field{0x35, 0, 8, false}},
+    {"GYRO_YOUT_L", Field{0x36, 0, 8, false}},
+    {"GYRO_ZOUT_H", Field{0x37, 0, 8, false}},
+    {"GYRO_ZOUT_L", Field{0x38, 0, 8, false}},
+    {"TEMP_OUT_H", Field{0x39, 0, 8, false}},
+    {"TEMP_OUT_L", Field{0x3A, 0, 8, false}},
+    {"GYRO_CONFIG_1", Field{0x01, 0, 8, false}},
+    {"GYRO_SMPLRT_DIV", Field{0x00, 0, 8, false}},
+    {"GYRO_DLPFCFG", Field{0x01, 3, 3, false}},
+    {"GYRO_FS_SEL", Field{0x01, 1, 2, false}},
+    {"GYRO_FCHOICE", Field{0x01, 0, 1, false}},
+    {"GYRO_CONFIG_2", Field{0x02, 0, 8, false}},
+    {"XGYRO_CTEN", Field{0x02, 5, 1, false}},
+    {"YGYRO_CTEN", Field{0x02, 4, 1, false}},
+    {"ZGYRO_CTEN", Field{0x02, 3, 1, false}},
+    {"GYRO_AVGCFG", Field{0x02, 0, 3, false}},
+    {"ACCEL_SMPLRT_DIV_1", Field{0x10, 0, 3, false}},
+    {"ACCEL_SMPLRT_DIV_2", Field{0x11, 0, 8, false}},
+    {"ACCEL_CONFIG", Field{0x14, 0, 8, false}},
+    {"ACCEL_DLPFCFG", Field{0x14, 3, 3, false}},
+    {"ACCEL_FS_SEL", Field{0x14, 1, 2, false}},
+    {"ACCEL_FCHOICE", Field{0x14, 0, 1, false}},
+    {"ACCEL_CONFIG_2", Field{0x15, 0, 8, false}},
+    {"AX_ST_EN_REG", Field{0x15, 4, 1, false}},
+    {"AY_ST_EN_REG", Field{0x15, 3, 1, false}},
+    {"AZ_ST_EN_REG", Field{0x15, 2, 1, false}},
+    {"DEC3_CFG", Field{0x15, 0, 2, false}},
+    {"TEMP_CONFIG", Field{0x53, 0, 8, false}},
+    {"TEMP_DLPFCFG", Field{0x53, 0, 3, false}},
+    {"REG_BANK_SEL", Field{0x7F, 0, 8, false}},
+    {"USER_BANK", Field{0x7F, 4, 2, false}},
+
+    // FIFO
+    {"FIFO_EN_2", Field{0x67, 0, 8, false}},
+    {"ACCEL_FIFO_EN", Field{0x67, 4, 1, false}},
+    {"GYRO_Z_FIFO_EN", Field{0x67, 3, 1, false}},
+    {"GYRO_Y_FIFO_EN", Field{0x67, 2, 1, false}},
+    {"GYRO_X_FIFO_EN", Field{0x67, 1, 1, false}},
+    {"TEMP_FIFO_EN", Field{0x67, 0, 1, false}},
+    {"FIFO_RST", Field{0x68, 0, 8, false}},
+    {"FIFO_RESET", Field{0x68, 0, 4, false}},
+    {"FIFO_MODE", Field{0x69, 0, 4, false}},
+    {"FIFO_COUNTH", Field{0x70, 0, 4, false}},
+    {"FIFO_COUNTL", Field{0x71, 0, 8, false}},
+    {"FIFO_R_W", Field{0x72, 0, 8, false}},
+    {"WOF_STATUS", Field{0x74, 8, 1, false}},
+    {"RAW_DATA_RDY", Field{0x74, 0, 4, false}},
+    {"FIFO_CFG", Field{0x76, 0, 1, false}},
+
+  };
 
 };
