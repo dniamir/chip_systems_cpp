dcd96c352b8a90c34e695fb3c7b1c04aaf52ea6e
Darien Niamir <darien.niamir@gmail.com>
2022-12-11 23:38:01 -0800

    First pass at code for MAX17260 fuel gauge

---
+++ max17260.cpp
@@ -0,0 +1,94 @@
+# include <max17260.h>
+# include <Arduino.h>
+# include <arduino_i2c.h>
+
+MAX17260::MAX17260(int i2c_address_in, ArduinoI2C input_protocol) : Chip(i2c_address_in, input_protocol) {
+    Chip::field_map = field_map;
+    Chip::who_am_i_reg = who_am_i_reg;
+};
+
+MAX17260::MAX17260(ArduinoI2C input_protocol) : Chip(input_protocol) {
+    Chip::field_map = field_map;
+    Chip::who_am_i_reg = who_am_i_reg;
+    Chip::i2c_address = MAX17260::i2c_address;
+};
+
+// void MAX17260::soft_reset() {
+//     MAX17260::read_field16("reset", 0xB6);
+// }
+
+bool MAX17260::check_por() {
+    return (bool)MAX17260::read_field16(0x00);
+}
+
+void MAX17260::configure_system() {
+
+    float ChargeVoltage = 3.7;
+
+    // https://pdfserv.maximintegrated.com/en/an/ug6595-modelgauge-m5-host-side-software-implementation-guide.pdf
+
+    // Make sure POR is 1
+    if (!MAX17260::check_por()) {
+        while(MAX17260::read_field16(0x3D) & 1) {delay(10);}
+    }
+
+    uint16_t HibCFG = MAX17260::read_field(0xBA);
+    MAX17260::write_field16(0x60, 0x90); // Exit Hibernate Mode step 1
+    MAX17260::write_field16(0xBA, 0x0); // Exit Hibernate Mode step 2
+    MAX17260::write_field16(0x60, 0x0); // Exit Hibernate Mode step 3
+
+    MAX17260::write_field16(0x18, DesignCap); // Write DesignCap
+    MAX17260::write_field16(0x1E, IchgTerm); // Write IchgTerm
+    MAX17260::write_field16(0x3A, VEmpty); // Write VEmpty
+    if (ChargeVoltage > 4.275) {
+        MAX17260::write_field16(0xDB, 0x8400); // Write ModelCFG
+    }
+    else {
+        MAX17260::write_field16(0xDB, 0x8000); // Write ModelCFG
+    }
+    //Poll ModelCFG.Refresh(highest bit),
+    //proceed to Step 3 when ModelCFG.Refresh=0.
+    while (MAX17260::read_field16(0xDB) & 0x8000) {delay(10);}
+
+    MAX17260::write_field16(0xBA, HibCFG); // Restore Original HibCFG value
+
+    // Clear POR Bit
+    uint16_t Status = MAX17260::read_field16("Status");
+    MAX17260::write_field16(0x00, Status & 0xFFFD);
+
+}
+
+float MAX17260::read_batt_voltage() {
+    return MAX17260::read_field16("VCell") * MAX17260::v_per_lsb;
+}
+
+float MAX17260::read_level_percent() {
+    return MAX17260::read_field16(0x06) * MAX17260::per_per_lsb;
+}
+
+float MAX17260::read_level_mahrs() {
+    return MAX17260::read_field16(0x05) * MAX17260::mah_per_lsb;
+}
+
+float MAX17260::read_tte() {
+    return MAX17260::read_field16(0x11) * MAX17260::sec_per_lsb;
+}
+
+void MAX17260::read_data(bool print_data) {
+
+    float level_percent = MAX17260::read_level_percent();
+    float level_mah = MAX17260::read_level_mahrs();
+    float time_to_empty = MAX17260::read_tte();
+
+    if (!print_data) {return;}
+
+    Serial.print("Fuel Gauge: ");
+    Serial.print(level_percent);
+    Serial.print("%, ");
+    Serial.print(level_mah);
+    Serial.print("mAh, ");
+    Serial.print(time_to_empty);
+    Serial.print("s");
+    Serial.println();
+
+}
---
+++ max17260.h
@@ -0,0 +1,115 @@
+# include <protocol.h>
+# include <Arduino.h>
+# include <arduino_i2c.h>
+# include <chip.h>
+
+
+class MAX17260 : public Chip {
+
+  public:
+    // Different constructors
+    MAX17260(ArduinoI2C input_protocol);
+    MAX17260(int i2c_address_in, ArduinoI2C input_protocol);
+
+    int i2c_address = 0x36;
+
+    // WHO AM I register - register the check upon startup
+    Field who_am_i_reg = Field{0x21, 0, 8, false};
+
+    void configure_system();
+    void soft_reset();  // Soft reset
+    bool check_por();
+
+    float read_level_percent();
+    float read_level_mahrs();
+    float read_tte();
+
+    float read_batt_voltage();
+
+    void read_data(bool print_data);
+
+
+  private:
+
+    // Value in default WHO AM I register - value to match during startup check
+    int who_am_i_val = 0x61;
+
+    uint16_t DesignCap = 0x1450;
+    uint16_t IchgTerm = 0x333;
+    uint16_t VEmpty = 0xa561;
+    // uint16_t DesignCap = 0x1450;
+
+    // Conversions
+    float sec_per_lsb = 5.625;
+    float per_per_lsb = 1 / 256;
+    float mah_per_lsb = 0.5;
+    float v_per_lsb =  78.125 * 1e-6;
+
+
+  protected:
+
+  public:
+
+  // I2C Register Map - SPI Map is different
+  std::map<String, Field> field_map {
+    
+    {"Status", Field{0x00, 0, 16, false}},
+    {"POR", Field{0x00, 1, 1, false}},
+    {"VAlrtTh", Field{0x01, 0, 16, false}},
+
+    {"VEmpty", Field{0x03A, 0, 16, false}},
+    {"VE", Field{0x03A, 7, 9, false}},
+    {"VR", Field{0x01, 0, 7, false}},
+    {"VAlrtTh", Field{0x01, 0, 16, false}},
+
+    {"ModelCFG", Field{0xDB, 0, 16, false}},
+    {"Refresh", Field{0xDB, 15, 1, false}},
+    {"R100", Field{0xDB, 13, 1, false}},
+    {"VChg", Field{0xDB, 10, 1, false}},
+    {"ModelID", Field{0xDB, 4, 4, false}},
+    {"CSEL", Field{0xDB, 2, 1, false}},
+
+    {"IChgTerm", Field{0x1E, 0, 16, false}},
+
+    {"Config", Field{0x1D, 0, 16, false}},
+    {"TSel", Field{0x1D, 15, 1, false}},
+    {"SS", Field{0x1D, 14, 1, false}},
+    {"TS", Field{0x1D, 13, 1, false}},
+    {"VS", Field{0x1D, 12, 1, false}},
+    {"IS", Field{0x1D, 11, 1, false}},
+    {"THSH", Field{0x1D, 10, 1, false}},
+    {"Ten", Field{0x1D, 9, 1, false}},
+    {"Tex", Field{0x1D, 8, 1, false}},
+    {"SHDN", Field{0x1D, 7, 1, false}},
+    {"COMMSH", Field{0x1D, 6, 1, false}},
+    {"ETHRM", Field{0x1D, 4, 1, false}},
+    {"FTHRM", Field{0x1D, 3, 1, false}},
+    {"Aen", Field{0x1D, 2, 1, false}},
+    {"Bei", Field{0x1D, 1, 1, false}},
+    {"Ber", Field{0x1D, 0, 1, false}},
+
+    {"Config2", Field{0xBB, 0, 16, false}},
+    {"AltRateEn", Field{0xBB, 13, 1, false}},
+    {"DPEn", Field{0xBB, 12, 1, false}},
+    {"POWR", Field{0xBB, 8, 4, false}},
+    {"dSOCen", Field{0xBB, 7, 1, false}},
+    {"TAIrtEn", Field{0xBB, 6, 1, false}},
+    {"LDMdl", Field{0xBB, 5, 1, false}},
+    {"DRCfg", Field{0xBB, 2, 2, false}},
+    {"CPMode", Field{0xBB, 1, 1, false}},
+
+    {"RepCap", Field{0x05, 0, 16, false}},
+    {"RepSOC", Field{0x06, 0, 16, false}},
+    {"FullCapRep", Field{0x10, 0, 16, false}},
+    {"TTE", Field{0x11, 0, 16, false}},
+    {"TTF", Field{0x20, 0, 16, false}},
+
+    {"VCell", Field{0x09, 0, 16, false}},
+    {"AvgVCell", Field{0x19, 0, 16, false}},
+    {"MaxMinVolt", Field{0xFF, 0, 16, false}},
+    {"MaxVCELL", Field{0xFF, 8, 8, false}},
+    {"MinVCELL", Field{0xFF, 0, 8, false}},
+
+  };
+
+};
